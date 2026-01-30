f8723dbe4a6279087651f4e380f98ad88b59699f
Dasten MA <150040523+ntijoh-Dasten-Mohamad-Amin@users.noreply.github.com>
2026-01-30 13:55:13 +0100

    added all todos (also sus)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,57 @@
-import { useState } from "react";
-import './App.css'
+import { useState} from "react";
+import "./App.css";
+import red_imposter from "./assets/red_imposter.jpeg";
+
+interface Todo {
+  id: number;
+  name: string;
+  completed: boolean;
+}
 
 function App() {
-  const [todos, setTodos] = useState<string[]>([]);
   const [input, setInput] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
     if (!input.trim()) return;
 
-    setTodos(prev => [...prev, input]);
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now(),
+        name: input,
+        completed: false,
+      },
+    ]);
+
     setInput("");
+  }
+
+  function handleDelete(id: number) {
+    setTodos(prev => prev.filter(todo => todo.id !== id));
+  }
+
+  function handlealert() {
+    alert('RAAAAUUUUUUGGGGGGGGHHHHHHHHH');
   }
 
   return (
     <>
+      <img src={red_imposter} alt="red susmongus" />
       <h1>Among Us</h1>
 
       <form onSubmit={handleSubmit}>
-        <input type="" value={input} onChange={e => setInput(e.target.value)}/>
-        <button>Add</button>
+        <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
+          <button>Add</button>
+          <button onClick={handlealert}> ALERT </button>
       </form>
 
       <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+        {todos.map(todo => (
+          <li key={todo.id}> <input type="checkbox" /> {todo.name}
+            <button onClick={() => handleDelete(todo.id)}> Delete </button>
+          </li>
         ))}
       </ul>
     </>
